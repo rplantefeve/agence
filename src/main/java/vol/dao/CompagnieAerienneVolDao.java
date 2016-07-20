b8fba07f54aab0773dbb0586dafc41b9f7662e41
TimotheedLT <timotheedlt@gmail.com>
2016-07-20 14:53:41 +0200

    Ajout commentaire Javadoc

--- src/main/java/vol/dao/CompagnieAerienneVolDao.java
+++ src/main/java/vol/dao/CompagnieAerienneVolDao.java
@@ -2,6 +2,11 @@ package vol.dao;
 
 import vol.model.CompagnieAerienneVol;
 
+/**
+ * permet de synchroniser l'objet métier et la BDD
+ * @author ajc
+ *
+ */
 public interface CompagnieAerienneVolDao extends Dao<CompagnieAerienneVol, Integer> {
 	
 	CompagnieAerienneVol findById (Integer id);
